6ab6bf029ebbfacb24027a8615823bf3f882911f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-05-26 16:34:47 +0200

    FIX French Timezone

--- graphite/local_settings.py
+++ graphite/local_settings.py
@@ -4,7 +4,7 @@
 #DEBUG = True
 
 # Set your local timezone (django will try to figure this out automatically)
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 # Setting MEMCACHE_HOSTS to be empty will turn off use of memcached entirely
 #MEMCACHE_HOSTS = ['127.0.0.1:11211']
